2f7ca1d333a43ed8c6d92f682c1534adcb39cf9c
Liberbell <liberbell@gmail.com>
2020-07-09 08:40:33 +0900

    methods4

--- methods.py
+++ methods.py
@@ -3,3 +3,6 @@ cars_list = ["Toyota Camry", "Honda Accord", "Honda Civic", "Toyota Corolla"]
 # cars_list[4] = "GM Buick"
 cars_list.append("GM Buick")
 print(cars_list)
+
+cars_list.append("Suzuki Vectora", "Nissan Pathfinder")
+print(cars_list)
