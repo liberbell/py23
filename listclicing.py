0e7fa651ec337c1f36ece3633c4b6198792e884d
Liberbell <liberbell@gmail.com>
2020-07-13 08:36:08 +0900

    listslicing1

--- listclicing.py
+++ listclicing.py
@@ -1,1 +1,4 @@
-my_cars_list = ["Toyota Corolla", "Toyota Camry", "Nissan Altima", "Honda Accord", "Honda Civic", ]
+my_cars_list = ["Toyota Corolla", "Toyota Camry", "Nissan Altima", "Honda Accord", "Honda Civic", "Toyota MarkX"]
+print(my_cars_list)
+
+print(len(my_cars_list))
